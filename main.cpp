087405c604e7d0e5a8224b9f09cd69001b0c87cf
Charlotte <charlotte.kruzic@hotmail.fr>
2024-04-10 19:17:35 +0200

    déplacement plateforme clavier

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#include "src/plateform.h"
+
 int main() {
   // Initialisation de la SDL2
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -21,14 +23,53 @@ int main() {
     return 1;
   }
 
+  // Création d'un renderer
+  SDL_Renderer* renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  if (renderer == nullptr) {
+    std::cerr << "Erreur lors de la création du renderer: " << SDL_GetError()
+              << std::endl;
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
+
+  int window_width, window_height;
+  SDL_GetWindowSize(window, &window_width, &window_height);
+
+  Plateform MyPlateform(window_width, window_height);
+  MyPlateform.render(renderer);
+  SDL_RenderPresent(renderer);
+
+  bool end = false;
+  SDL_Event event;
+
+  // boucle de jeu
+  while (!end) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        end = true;
+      } else if (event.type == SDL_KEYDOWN) {
+        std::cout << "Key pressed: " << event.key.keysym.sym << std::endl;
+        MyPlateform.move_keyboard(event.key.keysym.sym);
+      }
+    }
+
+    // Mise à jour affichage
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    MyPlateform.render(renderer);
+
+    SDL_RenderPresent(renderer);
+  }
+
   // Attendre 5 secondes
   // SDL_Delay(5000);
-  Grille maGrille(
-      "grille.txt");  // Remplacez "grille.txt" par le nom de votre fichier
-                      // ASCII contenant la grille de briques
-  maGrille.afficherGrille();
 
   // Nettoyage et fermeture de la SDL2
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
 
