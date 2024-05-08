80ec66b7a9e85ebac4f173c5409f9f87bbc41192
Zoé Marquis <zoe.marquis@etu.unistra.fr>
2024-05-08 22:03:20 +0200

    --help

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "Game.h"
 
@@ -10,7 +11,31 @@ int main(int argc, char* argv[]) {
 
   std::string nomFichierGrille = argv[1];
 
-  Game game(nomFichierGrille);  // Passer le nom du fichier à Game
+  // Vérifier si l'utilisateur demande de l'aide
+  std::string argument = argv[1];
+  if (argument == "--help") {
+    std::cout
+        << "Ce programme est un jeu de casse-briques.\n"
+        << "Pour jouer, veuillez fournir le nom du fichier de grille "
+        << "en tant qu'argument.\n"
+        << "Exemple d'utilisation: " << argv[0] << " grille.txt\n"
+        << "Commandes:\n"
+        << "\tAu clavier :\n"
+        << "\t - Utilisez les touches gauche et droite pour déplacer la "
+           "plateforme.\n"
+        << "\t - Appuyez sur la barre d'espace pour mettre en pause ou "
+           "reprendre le jeu.\n"
+        << "\tÀ la souris :\n"
+        << "\t - Utilisez la souris pour déplacer la plateforme et cliquez "
+           "pour mettre en pause ou reprendre le jeu.\n"
 
+        << "Programme réalisé par Charlotte Kruzic et Zoé Marquis dans "
+           "le cadre du cours de "
+        << "programmation avancée de Master 1 informatique - SDSC.\n";
+    return 0;
+  }
+
+  // Démarrer le jeu avec le nom du fichier de grille fourni
+  Game game(nomFichierGrille);
   return game.execute();
 }
